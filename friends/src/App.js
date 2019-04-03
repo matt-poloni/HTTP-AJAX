a5e5e9815be82cecca1e8eec95482d15765a0dee
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-04-03 15:40:35 -0400

    Eliminated need for CSS files other than reset.css

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
-import './App.css';
+import {createGlobalStyle} from 'styled-components';
+import './reset.css';
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    width: 100vw;
+    min-height: 100vh;
+    font-size: 1.6rem;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+    text-align:center;
+  }
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Hello World
-      </div>
+      <React.Fragment>
+        <GlobalStyle />
+        <div className="App">
+          Hello World
+        </div>
+      </React.Fragment>
     );
   }
 }
